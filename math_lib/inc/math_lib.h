9b69912652fdd290f31612bc4dff0dea5e00051f
Maks Bidolakh <102620153+MaksBid@users.noreply.github.com>
2025-02-07 14:05:05 +0200

    Triangle perimeter function

--- math_lib/inc/math_lib.h
+++ math_lib/inc/math_lib.h
@@ -1,1 +1,2 @@
 double triangle_area(double base, double height);
+double triangle_perimeter(double side1, double side2, double side3);
--- math_lib/src/math_lib.c
+++ math_lib/src/math_lib.c
@@ -3,3 +3,7 @@
 double triangle_area(double base, double height) {
     return 0.5 * base * height;
 }
+
+double triangle_perimeter(double side1, double side2, double side3) {
+    return side1 + side2 + side3;
+}
